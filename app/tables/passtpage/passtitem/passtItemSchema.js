49098547ff17f243a2e3152fd3c310ff04ff36e1
erubado <erubado@QXNB-367>
2025-04-22 15:00:32 -0300

    cambios

--- app/tables/passtpage/passtitem/passtItemSchema.js
+++ app/tables/passtpage/passtitem/passtItemSchema.js
@@ -1,28 +1,14 @@
 const mongoose = require('mongoose');
 
 const passtItemSchema = new mongoose.Schema({
-    generalObjectiveId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'GeneralObjective',
-        default: null
+    generalObjective: {
+        type: String,
+        required: true
     },
 
-    specificObjectiveId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'SpecificObjective',
-        default: null
-    },
-
-    managementToolId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'ManagementTool',
-        default: null
-    },
-
-    activityId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Activity',
-        default: null
+    specificObjective: {
+        type: String,
+        required: true
     },
 
     metas: [{
@@ -44,41 +30,14 @@ const passtItemSchema = new mongoose.Schema({
         type: String,
         required: true,
     }],
-
-    companyAreaId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'companyArea',
-        default: null,
-        required: true,
-    },
-
-    charge: {
-        type: String,
-        required: true,
-    },
-
-    companyDestinatedAreaId: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'companyArea',
-        default: null,
-        required: true,
-    }],
-
-    mounthsForExecute: [{
-        type:Number,
-        min: 1,
-        max:12,
-        required:true
-    }
-    ],
-
-    mounthsExecuted: [{
-        type:Number,
-        min: 1,
-        max:12,
-        required:false
-    }
-    ],
+ 
+    passtItemActivites: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'passtItemActivity',
+            default: null,
+            required: true,
+        }],
+    
 
 });
 
